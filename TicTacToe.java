33031301fe7176ea2012b045fc42ca5a9c20640c
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-07 11:06:54 +0530

    Added player will give input x and 0 and will print value

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,21 +6,34 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
        TicTacToe t =new TicTacToe();
-       t.uc1();
+       t.Uc1();
+       t.Uc2();
 
     }
 
 
-public static void uc1(){
+public static void Uc1(){
     String [] board = new String[10];
     for(int i=1; i<board.length;i++){
         board[i] = String.valueOf(i);
     }
     System.out.println(Arrays.toString(board));
 }
-
-
-
+    public static void Uc2(){
+        System.out.println("choose your letter for playing");
+        Scanner sc = new Scanner(System.in);
+        String playLetter=sc.next();
+        if(playLetter.equals("x")){
+            System.out.println("you choose x\nComputer will choose 0");
+        }else if(playLetter.equals("0")){
+            System.out.println("you choose 0\ncomputer will choose x");
+        }else {
+            System.out.println("invalid input");
+            Uc2();
+        }
+    }
 }
+
+
 
 
