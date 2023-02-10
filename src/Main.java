402c396eff2a30b7529a233f1da3d221c6b011b1
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-10 20:57:13 +0530

    added moving user and play it

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static String [] board = new String[10];
+    static String playLetter = null;
     public static void main(String[] args) {
-        System.out.println("Here Start Tic tac toe program");
         UC1();
         UC2();
-        UC3ShowBoard();
+        UC4();
 
     }
     public static void UC1(){
@@ -18,7 +18,7 @@ public class Main {
     }
     public static void UC2(){
         System.out.println("Please choose your letter for playing (X or O) \ntype letter & press enter");
-        String playLetter = sc.next();
+        playLetter = sc.next();
         if(playLetter.equals("x") ){
             System.out.println("You choose X \nnow computer will play with O");
         } else if(playLetter.equals("o")){
@@ -34,6 +34,24 @@ public class Main {
         System.out.println("| " +board[4] + " | " +board[5]+ " | " +board[6]+ " |\n-------------");
         System.out.println("| " +board[7] + " | " +board[8]+ " | " +board[9]+ " |\n-------------");
     }
+    public static void UC4(){
+        System.out.println("");
+        UC3ShowBoard();
+        System.out.println("Enter a slot number to Enter "+playLetter);
+        int input = sc.nextInt();
+
+        switch (input) {
+            case 1, 2, 3, 4, 5, 6, 7, 8, 9:
+                board[input] = playLetter;
+                UC3ShowBoard();
+                break;
+            default:
+                System.out.println("Invalid slot Number. please Enter valid slot Number");
+                UC4();
+                break;
+
+        }
 
 
+    }
 }
